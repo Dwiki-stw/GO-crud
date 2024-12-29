6d085bd88698b49e60685bf724228669b2bc0380
setw <dwiki.stw12@gmail.com>
2024-12-29 22:05:32 +0700

    configure database

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"go-product-categories/config"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.ConnectDB()
+
 	log.Println("Server Running on Port 8080")
 	http.ListenAndServe(":8080", nil)
 }
