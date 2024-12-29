87c82b79f903bd2aea03110d41f3a5150c835736
setw <dwiki.stw12@gmail.com>
2024-12-29 22:43:03 +0700

    init home page

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"go-product-categories/config"
+	"go-product-categories/controllers/homecontroller"
 	"log"
 	"net/http"
 )
 
 func main() {
 	config.ConnectDB()
+
+	//homepage
+	http.HandleFunc("/", homecontroller.Welcome)
 
 	log.Println("Server Running on Port 8080")
 	http.ListenAndServe(":8080", nil)
